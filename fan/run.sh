a2859c5a0cfbc376288cc6f27f78e016fb74809e
Philippe Vanhaesendonck <philippe.vanhaesendonck@e-bulles.be>
2018-03-31 17:38:43 +0000

    Fan: add status file

--- fan/run.sh
+++ fan/run.sh
@@ -5,21 +5,26 @@ Params=""
 
 if [ -n "${GW_TARGET_TEMP}" ]
 then
-  Params="--target-temp ${GW_TARGET_TEMP}"
+  Params="${Params} --target-temp ${GW_TARGET_TEMP}"
 fi
 
 if [ -n "${GW_INFLUXDB_HOST}" ]
 then
-  sed -i "s/^influxdb_host = .*/influxdb_host = '${GW_INFLUXDB_HOST}'/" ./ttn-fan/ttn-fan.py
+  sed -i "s/^    influxdb_host = .*/    influxdb_host = '${GW_INFLUXDB_HOST}'/" ./ttn-fan/ttn-fan.py
+  Params="${Params} --influxdb"
 fi
 
 if [ -n "${GW_INFLUXDB_DBNAME}" ]
 then
-  sed -i "s/^influxdb_dbname = .*/influxdb_dbname = '${GW_INFLUXDB_DBNAME}'/" ./ttn-fan/ttn-fan.py
+  sed -i "s/^    influxdb_dbname = .*/    influxdb_dbname = '${GW_INFLUXDB_DBNAME}'/" ./ttn-fan/ttn-fan.py
 fi
 
 echo "*** Starting TTN Fan control"
 echo "*** InfluxDB host     : ${GW_INFLUXDB_HOST:-Default}"
 echo "*** InfluxDB DB name  : ${GW_INFLUXDB_DBNAME:-Default}"
 echo "*** Target temperature: ${GW_TARGET_TEMP:-Default}"
-exec python -u ./ttn-fan/ttn-fan.py --user-mode --lock-file /var/ttn/sht21.lock ${Params}
+exec python -u ./ttn-fan/ttn-fan.py \
+        --user-mode \
+        --lock-file /var/ttn/sht21.lock \
+        --status-file /var/ttn/fan-stat.json \
+        ${Params}
