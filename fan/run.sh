5bd9f010bced675ffcea66714ad8636bd6d0a968
Philippe Vanhaesendonck <philippe.vanhaesendonck@e-bulles.be>
2018-03-31 09:03:26 +0000

    Add fan control container

---
+++ fan/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Setup environment and start ttn-fan control loop
+
+Params=""
+
+if [ -n "${GW_TARGET_TEMP}" ]
+then
+  Params="--target-temp ${GW_TARGET_TEMP}"
+fi
+
+if [ -n "${GW_INFLUXDB_HOST}" ]
+then
+  sed -i "s/^influxdb_host = .*/influxdb_host = '${GW_INFLUXDB_HOST}'/" ./ttn-fan/ttn-fan.py
+fi
+
+if [ -n "${GW_INFLUXDB_DBNAME}" ]
+then
+  sed -i "s/^influxdb_dbname = .*/influxdb_dbname = '${GW_INFLUXDB_DBNAME}'/" ./ttn-fan/ttn-fan.py
+fi
+
+echo "*** Starting TTN Fan control"
+echo "*** InfluxDB host     : ${GW_INFLUXDB_HOST:-Default}"
+echo "*** InfluxDB DB name  : ${GW_INFLUXDB_DBNAME:-Default}"
+echo "*** Target temperature: ${GW_TARGET_TEMP:-Default}"
+exec python -u ./ttn-fan/ttn-fan.py --user-mode --lock-file /var/ttn/sht21.lock ${Params}
