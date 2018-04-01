e23aa196c97d4d451bad932c7e424dbe3958f99f
Philippe Vanhaesendonck <philippe.vanhaesendonck@e-bulles.be>
2018-04-01 15:03:56 +0000

    Cleanup environment variables

--- collectd/run.sh
+++ collectd/run.sh
@@ -5,31 +5,47 @@ Config="/etc/collectd/collectd.conf"
 ConfigNetwork="/etc/collectd/collectd.conf.d/network.conf"
 ConfigPython="/etc/collectd/collectd.conf.d/python.conf"
 
+echo "*** Starting TTN Collectd daemon"
+
 if [ "${RESIN}" = "1" ]
 then
- # Resin environement
- # Set hostname, we take GW_ID, or Resin device name
- HostName="${GW_ID:-$RESIN_DEVICE_NAME_AT_INIT}"
- echo "Setting hostname to ${HostName}"
- sed -i "s/^FQDNLookup .*/FQDNLookup false/" "${Config}"
- sed -i "s/^#Hostname .*/Hostname \"${HostName}\"/" "${Config}"
+  echo "*** Running in resin.io environment"
+
+  # Expunge unexpanded variables from docker-compose
+  GwVars=$(compgen -A variable GW_)
+  for GwVar in ${GwVars}
+  do
+    [[ ${!GwVar} == \$\{* ]] && unset ${GwVar}
+  done
+  unset GwVars
+
+  # Set hostname, we take GW_ID, or Resin device name
+  HostName="${GW_ID:-$RESIN_DEVICE_NAME_AT_INIT}"
+  echo "*** Setting hostname to ${HostName}"
+  sed -i "s/^FQDNLookup .*/FQDNLookup false/" "${Config}"
+  sed -i "s/^#Hostname .*/Hostname \"${HostName}\"/" "${Config}"
 fi
 
 if [ -n "${GW_COLLECTD_SERVER}" ]
 then
+  echo "*** Collectd server: ${GW_COLLECTD_SERVER}"
   sed -i "s/^\tServer .*/\tServer \"${GW_COLLECTD_SERVER}\"/" "${ConfigNetwork}"
+else
+  echo "*** No Collectd server specified"
+  exec sleep 86400
 fi
 
 if [ "${GW_BACKPLANE}" = "DBRGN" ]
 then
-  # Dbrgn's backplane: Enable voltage, temperature, humidity collection
+  echo "*** Support for Dbrgn's backplane enabled"
+  # Enable voltage, temperature, humidity collection
   sed -i 's/##SHT21## //' "${ConfigPython}"
   sed -i 's/##MCP3425## //' "${ConfigPython}"
 fi
 
 if [ "${GW_TTN_FAN}" = "true" ]
 then
-  # Fan monitor
+  echo "*** Fan monitoring enabled"
   sed -i 's/##TTN_FAN## //' "${ConfigPython}"
 fi
 
--- fan/run.sh
+++ fan/run.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 # Setup environment and start ttn-fan control loop
 
+echo "*** Starting TTN Fan control"
+
+if [ "${RESIN}" = "1" ]
+then
+  echo "*** Running in resin.io environment"
+
+  # Expunge unexpanded variables from docker-compose
+  GwVars=$(compgen -A variable GW_)
+  for GwVar in ${GwVars}
+  do
+    [[ ${!GwVar} == \$\{* ]] && unset ${GwVar}
+  done
+  unset GwVars
+fi
+
 Params=""
+
+if [ "${GW_TTN_FAN}" != "true" ]
+then
+  echo "*** TTN Fan not enabled"
+  exec sleep 86400
+fi
 
 if [ -n "${GW_TARGET_TEMP}" ]
 then
@@ -19,7 +40,6 @@ then
   sed -i "s/^    influxdb_dbname = .*/    influxdb_dbname = '${GW_INFLUXDB_DBNAME}'/" ./ttn-fan/ttn-fan.py
 fi
 
-echo "*** Starting TTN Fan control"
 echo "*** InfluxDB host     : ${GW_INFLUXDB_HOST:-Default}"
 echo "*** InfluxDB DB name  : ${GW_INFLUXDB_DBNAME:-Default}"
 echo "*** Target temperature: ${GW_TARGET_TEMP:-Default}"
