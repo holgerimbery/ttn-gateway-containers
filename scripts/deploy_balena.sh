e4aa92e72fc0f7896facfa068ca5905f4ddff2a7
Philippe Vanhaesendonck <philippe.vanhaesendonck@e-bulles.be>
2019-05-15 20:29:02 +0200

    CI: deploy to balenaCloud

---
+++ scripts/deploy_balena.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Push images to the docker hub
+
+# Exit on error
+set -ev
+
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ]
+then
+  # Deploy on balena.io (Only if we have a deploy key)
+  if [ -n "${BALENA_DEPLOY_KEY}" ]
+  then
+    # Setup keys & fingerprints
+    eval $(ssh-agent -s)
+    echo -e "${BALENA_DEPLOY_KEY}" > id_rsa
+    chmod 0600 id_rsa
+    ssh-add ./id_rsa
+    ssh-keyscan git.balena-cloud.com >> ~/.ssh/known_hosts
+    # Monitoring backend to deploy: prometheus / collectd
+    cp "docker-compose-${MONITORNG_BACKEND:-collectd}.yml" docker-compose.yml
+    git add .
+    git commit -m "Select monitoring backend for the balenaCloud images"
+    if [ "${TRAVIS_BRANCH}" == "master" ]
+    then
+      # Push to all production apps
+      for REMOTE in ${!BALENA_PROD_*}
+      do
+	git remote add balena ${!REMOTE}
+	git push --force balena master
+	git remote remove balena
+      done
+    else
+      # Push to all dev apps
+      for REMOTE in ${!BALENA_DEV_*}
+      do
+	git remote add balena ${!REMOTE}
+	git push --force balena master
+	git remote remove balena
+      done
+    fi
+  fi
+fi
